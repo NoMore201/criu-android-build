13b178a7a4be1af5184da2276327ce76ef41d4a6
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-06-06 18:06:26 +0200

    Update criu build script
    Signed-off-by: Domenico Iezzi <domenico.iezzi.201@gmail.com>

--- criu/build-criu.sh
+++ criu/build-criu.sh
@@ -4,6 +4,7 @@ export ROOT=`pwd`
 
 PROTOBUF_URL="https://github.com/google/protobuf/archive/v3.3.0.tar.gz"
 PROTOBUFC_URL="https://github.com/protobuf-c/protobuf-c/archive/v1.2.1.tar.gz"
+CRIU_URL="https://github.com/xemul/criu/archive/v3.1.tar.gz"
 NDK_URL="https://dl.google.com/android/repository/android-ndk-r14b-linux-x86_64.zip"
 
 PBUF_DIR="protobuf"
@@ -13,8 +14,8 @@ TARGET="arm"
 
 # Preparing NDK build environment
 [[ ! -e ndk.zip ]] && curl -L $NDK_URL > ndk.zip
-[[ ! -d ./ndk ]] && unzip ndk.zip -d tmp && d=(tmp/*) && mv $d ./ndk && rm -rf tmp/
-[[ ! -d ./cross ]] && ndk/build/tools/make-standalone-toolchain.sh \
+[[ ! -d ndk ]] && unzip ndk.zip -d tmp && d=(tmp/*) && mv $d ./ndk && rm -rf tmp/
+[[ ! -d cross ]] && ndk/build/tools/make-standalone-toolchain.sh \
     --arch=arm --install-dir=$ROOT/cross \
     --platform=android-24
 
@@ -41,6 +42,7 @@ if [[ ! -d $PBUF_DIR ]]; then
 	make -j$(nproc) && make install
 fi
 
+export PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig
 cd $ROOT
 if [[ ! -d $PBUFC_DIR ]]; then
 	curl -L $PROTOBUFC_URL > $PBUFC_DIR.tar.gz
@@ -48,25 +50,24 @@ if [[ ! -d $PBUFC_DIR ]]; then
 	tar --strip=1 -xzvf $PBUFC_DIR.tar.gz -C $PBUFC_DIR
 	cd $ROOT/$PBUFC_DIR
 	./autogen.sh
+	CPPFLAGS=`pkg-config --cflags protobuf` \
+	LDFLAGS=`pkg-config --libs protobuf` \
 	./configure --prefix=$BUILD_DIR \
 		--host=arm-linux-androideabi \
-		--disable-protoc \
-		--disable-shared --enable-static \
-		--with-sysroot=$SYSROOT \
-		LDFLAGS="-L/usr/local/lib -llog" \
-		CXXFLAGS="-I/usr/local/include"
+		--disable-shared --enable-static --disable-protoc
 	make -j$(nproc) && make install
 fi
-exit
 
 cd $ROOT
-rm -rf criu
-mkdir criu
-tar --strip=1 -xzvf v2.12.1.tar.gz -C criu
-cd $ROOT/criu
-rm images/google/protobuf/descriptor.proto
-cp $ROOT/arm/include/google/protobuf/descriptor.proto images/google/protobuf/
-make -j$(nproc) \
-  ARCH=arm \
-  CROSS_COMPILE=$NDK_ROOT/bin/arm-linux-androideabi- \
-  USERCFLAGS="-I${ROOT}/arm/include -I${NDK_ROOT}/sysroot/usr/include -L${ROOT}/arm/lib"
+if [[ ! -d $CRIU_DIR ]]; then
+	curl -L $CRIU_URL > $CRIU_DIR.tar.gz
+	mkdir $CRIU_DIR
+	tar --strip=1 -xzvf $CRIU_DIR.tar.gz -C $CRIU_DIR
+	cd $ROOT/$CRIU_DIR
+	make -j$(nproc) \
+		ARCH=arm \
+		CROSS_COMPILE=$NDK_ROOT/bin/arm-linux-androideabi- \
+		USERCFLAGS="-I${BUILD_DIR}/include -L${BUILD_DIR}/lib" \
+		criu
+fi
+
